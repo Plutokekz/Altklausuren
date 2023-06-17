d0465ed04fec76b90607154848c5d93783f59e87
Lukas Mahr <lukas@yousuckatprogramming.de>
2023-06-18 00:59:13 +0200

    Add CORSMiddleware, get on or all courses, get one or all Semsters and implemented the body of upload file

--- server/main.py
+++ server/main.py
@@ -1,14 +1,25 @@
+import os
 from typing import List
 
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, UploadFile
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 import schemas
 from server.database import SessionLocal
 import crud
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_session():
@@ -19,7 +30,7 @@ def get_session():
         db.close()
 
 
-@app.post("examen/add", response_model=schemas.Examen)
+@app.post("/examen/add", response_model=schemas.Examen)
 async def add_examen(examen: schemas.Examen, session: Session = Depends(get_session)):
     examen, error = crud.add_examen(examen, session)
     if error is not None:
@@ -27,9 +38,9 @@ async def add_examen(examen: schemas.Examen, session: Session = Depends(get_sess
     return examen
 
 
-@app.post("examen/update/{examen_id}", response_model=schemas.Examen)
+@app.post("/examen/update/{examen_id}", response_model=schemas.Examen)
 async def update_examen(
-        examen: schemas.Examen, session: Session = Depends(get_session)
+    examen: schemas.Examen, session: Session = Depends(get_session)
 ):
     examen, error = crud.update_examen(examen, session)
     if error is not None:
@@ -37,7 +48,7 @@ async def update_examen(
     return examen
 
 
-@app.get("examen/get/{examen_id}", response_model=schemas.Examen)
+@app.get("/examen/get/{examen_id}", response_model=schemas.Examen)
 async def get_examen(examen_id: str, session: Session = Depends(get_session)):
     examen = crud.get_exman(examen_id, session)
     if examen is None:
@@ -47,32 +58,83 @@ async def get_examen(examen_id: str, session: Session = Depends(get_session)):
     return examen
 
 
-@app.get("examen/get/all", response_model=List[schemas.Examen])
-async def get_all_examen(count: int = 100, offset: int = 0, session: Session = Depends(get_session)):
+@app.get("/examen/all", response_model=List[schemas.Examen])
+async def get_all_examen(
+    count: int = 100, offset: int = 0, session: Session = Depends(get_session)
+):
     examen = crud.get_all_examens(session, count, offset)
     if examen is None or len(examen) == 0:
         raise HTTPException(
-            status_code=404, detail=f"Error no examen available"
+            status_code=404,
+            detail=f"Error no examen available with count {count} and offset {offset} found",
         )
     return examen
 
 
-@app.get("file/get/{file_id}", response_model=schemas.File)
+@app.get("/file/get/{file_id}", response_model=schemas.File)
 async def get_file(file_id: str, session: Session = Depends(get_session)):
     file = crud.get_file(file_id, session)
     if file is None:
-        raise HTTPException(
-            status_code=404, detail=f"Error no file id {file_id} found"
-        )
+        raise HTTPException(status_code=404, detail=f"Error no file id {file_id} found")
     return file
 
 
-@app.post("file/add", response_model=schemas.File)
-async def add_file(file_metadata: schemas.File, file: UploadFile, session: Session = Depends(get_session)):
+@app.post("/file/add", response_model=schemas.File)
+async def add_file(
+    file_metadata: schemas.File,
+    file: UploadFile,
+    session: Session = Depends(get_session),
+):
     file, error = crud.add_file(file_metadata, file, session)
     if error is not None:
         raise HTTPException(status_code=404, detail=error)
     return file
+
+
+@app.get("/course/get/{course_id}", response_model=schemas.Course)
+async def get_course(course_id: str, session: Session = Depends(get_session)):
+    course = crud.get_course(course_id, session)
+    if course is None:
+        raise HTTPException(
+            status_code=404, detail=f"Error no course id {course_id} found"
+        )
+    return course
+
+
+@app.get("/course/all", response_model=List[schemas.Course])
+async def get_all_courses(
+    count: int = 100, offset: int = 0, session: Session = Depends(get_session)
+):
+    courses = crud.get_all_courses(session, count, offset)
+    if courses is None or len(courses) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Error no courses available with offset {offset} and count {count}",
+        )
+    return courses
+
+
+@app.get("/semester/get/{semester_id}", response_model=schemas.Semester)
+async def get_semester(semester_id: str, session: Session = Depends(get_session)):
+    semester = crud.get_semester(semester_id, session)
+    if semester is None:
+        raise HTTPException(
+            status_code=404, detail=f"Error no semester id {semester_id} found"
+        )
+    return semester
+
+
+@app.get("/semester/all", response_model=List[schemas.Semester])
+async def get_all_semester(
+    count: int = 100, offset: int = 0, session: Session = Depends(get_session)
+):
+    semesters = crud.get_all_semester(session, count, offset)
+    if semesters is None or len(semesters) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Error no semesters available with offset {offset} and count {count}",
+        )
+    return semesters
 
 
 if __name__ == "__main__":
